92d849e64bac8152809637f2829d894dcb69b0a3
antonio <cekovski.a96@gmail.com>
2020-04-12 22:01:23 +0200

    trololo

---
+++ Student.java
@@ -0,0 +1,42 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+	int []grades;
+	
+	//TODO constructor
+	public Student(String index, String firstName, String lastName) {
+		// TODO Auto-generated constructor stub
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getAverage() {
+		//TODO
+	}
+
+	public int ECTSCredits() {
+		//TODO
+	}
+}
